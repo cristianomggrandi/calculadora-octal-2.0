e0b6cfd9b320e03d1a7f1d6a9953138f51f11fc7
Cristiano Grandi <80713582+cristianomggrandi@users.noreply.github.com>
2023-10-11 11:41:48 -0300

    Finalizar função de cálculo

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const resultado = document.getElementById("calculadora-resultado")
+const conta = document.getElementById("calculadora-conta")
 
 const botoesNumerais = document.getElementsByClassName("calculadora-botoes-numero")
 
 for (botao of botoesNumerais) {
     botao.addEventListener("click", e => {
-        const resultadoNumerico = Number(resultado.textContent)
+        resultado.textContent = (resultado.textContent !== "0" ? resultado.textContent : "") + e.target.textContent
+    })
+}
 
-        resultado.textContent = resultadoNumerico + Number(e.target.textContent)
+const botoesOperadores = document.getElementsByClassName("calculadora-botoes-operacao")
+
+for (botao of botoesOperadores) {
+    botao.addEventListener("click", e => {
+        const simbolo = e.target.textContent
+
+        if (["+", "-", "⨯", "÷"].includes(simbolo)) {
+            conta.textContent = conta.textContent + resultado.textContent + simbolo
+
+            resultado.textContent = "0"
+        } else {
+            if (simbolo === "=") {
+                const stringConta = (conta.textContent + resultado.textContent).replaceAll("⨯", "*").replaceAll("÷", "/")
+
+                resultado.textContent = eval(stringConta)
+                conta.textContent = ""
+            }
+        }
     })
 }
