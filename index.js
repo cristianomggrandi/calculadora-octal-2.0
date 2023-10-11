246a11cf8e9284169c667f8ca878b683d98c2978
Cristiano Grandi <80713582+cristianomggrandi@users.noreply.github.com>
2023-10-11 12:24:07 -0300

    Adicionar vírgula e separar classes de botões

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const resultado = document.getElementById("calculadora-resultado")
 const conta = document.getElementById("calculadora-conta")
 
-const botoesNumerais = document.getElementsByClassName("calculadora-botoes-numero")
+const botoesNumerais = document.getElementsByClassName("calculadora-botao-numero")
 
 for (botao of botoesNumerais) {
     botao.addEventListener("click", e => {
@@ -9,23 +9,29 @@ for (botao of botoesNumerais) {
     })
 }
 
-const botoesOperadores = document.getElementsByClassName("calculadora-botoes-operacao")
+const botoesOperadores = document.getElementsByClassName("calculadora-botao-operacao")
 
 for (botao of botoesOperadores) {
     botao.addEventListener("click", e => {
         const simbolo = e.target.textContent
 
-        if (["+", "-", "⨯", "÷"].includes(simbolo)) {
-            conta.textContent = conta.textContent + resultado.textContent + simbolo
+        conta.textContent = conta.textContent + resultado.textContent + simbolo
 
-            resultado.textContent = "0"
-        } else {
-            if (simbolo === "=") {
-                const stringConta = (conta.textContent + resultado.textContent).replaceAll("⨯", "*").replaceAll("÷", "/")
-
-                resultado.textContent = eval(stringConta)
-                conta.textContent = ""
-            }
-        }
+        resultado.textContent = "0"
     })
 }
+
+const botaoIgual = document.getElementsByClassName("calculadora-botao-igual")[0]
+
+botaoIgual.addEventListener("click", e => {
+    const stringConta = (conta.textContent + resultado.textContent).replaceAll("⨯", "*").replaceAll("÷", "/")
+
+    resultado.textContent = eval(stringConta)
+    conta.textContent = ""
+})
+
+const botaoVirgula = document.getElementsByClassName("calculadora-botao-virgula")[0]
+
+botaoVirgula.addEventListener("click", e => {
+    if (!resultado.textContent.includes(".")) resultado.textContent = resultado.textContent + "."
+})
